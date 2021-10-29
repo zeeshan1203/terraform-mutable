341c34d573dd24532f5605d2786c60c2d76e421e
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-29 17:46:20 +0530

    updated and modified

---
+++ vpc/route-tables.tf
@@ -0,0 +1,16 @@
+resource "aws_route_table" "private-rt" {
+  vpc_id          = aws_vpc.main.id
+
+  tags            = {
+    Name          = "private-route-table"
+  }
+}
+
+resource "aws_route_table" "public-rt" {
+  vpc_id          = aws_vpc.main.id
+
+  tags            = {
+    Name          = "private-route-table"
+  }
+}
+
