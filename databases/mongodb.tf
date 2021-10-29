8f7f9d15e010e6fd46f3b29fe43580adf01a64dc
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-29 20:26:00 +0530

    added & updated and modified

--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -1,37 +1,45 @@
-#resource "aws_spot_instance_request" "mongodb" {
-#  ami                         = data.aws_ami.centos7.id
-#  spot_price                  = "0.0031"
-#  instance_type               = "t3.micro"
-#  vpc_security_group_ids      = []
-#
-#  tags                        = {
-#    Name                      = ""
-#  }
-#}
-#
-#resource "aws_security_group" "allow_mongodb" {
-#  name                        = "allow_mongodb"
-#  description                 = "AllowMongoDB"
-#  vpc_id                      = aws_vpc.main.id
-#
-#  ingress {
-#    description      = "TLS from VPC"
-#    from_port        = 443
-#    to_port          = 443
-#    protocol         = "tcp"
-#    cidr_blocks      = [aws_vpc.main.cidr_block]
-#    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-#  }
-#
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = ["::/0"]
-#  }
-#
-#  tags = {
-#    Name = "allow_tls"
-#  }
-#}
+//resource "aws_spot_instance_request" "mongodb" {
+//  ami                         = data.aws_ami.centos7.id
+//  spot_price                  = "0.0031"
+//  instance_type               = "t3.micro"
+//  vpc_security_group_ids      = []
+//
+//  tags                        = {
+//    Name                      = ""
+//  }
+//}
+//
+//
+resource "aws_security_group" "allow_mongodb" {
+  name                        = "allow_mongodb"
+  description                 = "AllowMongoDB"
+  vpc_id                      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description               = "SSH"
+    from_port                 = 22
+    to_port                   = 22
+    protocol                  = "tcp"
+    cidr_blocks               = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
+  ingress {
+    description               = "MONGODB"
+    from_port                 = 27017
+    to_port                   = 27017
+    protocol                  = "tcp"
+    cidr_blocks               = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+  }
+
+  egress {
+    from_port                 = 0
+    to_port                   = 0
+    protocol                  = "-1"
+    cidr_blocks               = ["0.0.0.0/0"]
+    ipv6_cidr_blocks          = ["::/0"]
+  }
+
+  tags                        = {
+    Name                      = "AllowMongoDB"
+  }
+}
