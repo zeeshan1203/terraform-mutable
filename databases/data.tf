40147100312bcb551ca2ebcab0a161caa4c2ce36
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-29 20:03:31 +0530

    added & updated and modified

---
+++ databases/data.tf
@@ -0,0 +1,9 @@
+data "aws_ami" "centos7" {
+  most_recent      = true
+  name_regex       = "^Centos-7-DevOps-Practice"   ##ami name
+  owners           = ["973714476881"]              ##mai owner id
+}
+
+output "ami" {
+  value = data.aws_ami.centos7
+}
