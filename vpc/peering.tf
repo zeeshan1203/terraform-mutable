05af8fde60261078d5409060ad042004f3975258
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-29 14:22:44 +0530

    updated and modified

--- vpc/peering.tf
+++ vpc/peering.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc_peering_connection" "peer-connection" {
-  peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = var.DEFAULT_VPC_ID
+  peer_vpc_id         = aws_vpc.main.id
+  vpc_id              = var.DEFAULT_VPC_ID
+  auto_accept         = true
 }
