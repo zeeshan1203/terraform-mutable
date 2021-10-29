0ae0871d048d5d3ccb0d1a5a2fff952c6d6bf2d1
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-29 15:33:53 +0530

    updated and modified

--- vpc/vars.tf
+++ vpc/vars.tf
@@ -1,7 +1,8 @@
-variable "VPC_CIDR"               {}
-variable "ENV"                    {}
-variable "PUBLIC_SUBNETS_CIDR"    {}
-variable "PRIVATE_SUBNETS_CIDR"   {}
-variable "SUBNET_ZONES"           {}
-variable "DEFAULT_VPC_ID"         {}
+variable "VPC_CIDR"                {}
+variable "ENV"                     {}
+variable "PUBLIC_SUBNETS_CIDR"     {}
+variable "PRIVATE_SUBNETS_CIDR"    {}
+variable "SUBNET_ZONES"            {}
+variable "DEFAULT_VPC_ID"          {}
 variable "DEFAULT_VPC_ROUTE_TABLE" {}
+variable "DEFAULT_VPC_CIDR"        {}
