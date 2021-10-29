558b4d25d5ed869e2f79c2745e7a246a7620279f
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-29 20:23:36 +0530

    added & updated and modified

--- databases/data.tf
+++ databases/data.tf
@@ -3,3 +3,17 @@ data "aws_ami" "centos7" {
   name_regex       = "^Centos-7-DevOps-Practice"   ##ami name
   owners           = ["973714476881"]              ##mai owner id
 }
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket                = "terraform-szs"       	##ur bucket name
+    key                   = "mutable/vpc/${var.ENV}/terraform.tfstate"
+    region                = "us-east-1"
+  }
+}
+
+output "outputs" {
+  value = data.terraform_remote_state.vpc.outputs
+}
--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -1,37 +1,37 @@
-resource "aws_spot_instance_request" "mongodb" {
-  ami                         = data.aws_ami.centos7.id
-  spot_price                  = "0.0031"
-  instance_type               = "t3.micro"
-  vpc_security_group_ids      = []
-
-  tags                        = {
-    Name                      = ""
-  }
-}
-
-resource "aws_security_group" "allow_mongodb" {
-  name                        = "allow_mongodb"
-  description                 = "AllowMongoDB"
-  vpc_id                      = aws_vpc.main.id
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
+#resource "aws_spot_instance_request" "mongodb" {
+#  ami                         = data.aws_ami.centos7.id
+#  spot_price                  = "0.0031"
+#  instance_type               = "t3.micro"
+#  vpc_security_group_ids      = []
+#
+#  tags                        = {
+#    Name                      = ""
+#  }
+#}
+#
+#resource "aws_security_group" "allow_mongodb" {
+#  name                        = "allow_mongodb"
+#  description                 = "AllowMongoDB"
+#  vpc_id                      = aws_vpc.main.id
+#
+#  ingress {
+#    description      = "TLS from VPC"
+#    from_port        = 443
+#    to_port          = 443
+#    protocol         = "tcp"
+#    cidr_blocks      = [aws_vpc.main.cidr_block]
+#    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+#  }
+#
+#  egress {
+#    from_port        = 0
+#    to_port          = 0
+#    protocol         = "-1"
+#    cidr_blocks      = ["0.0.0.0/0"]
+#    ipv6_cidr_blocks = ["::/0"]
+#  }
+#
+#  tags = {
+#    Name = "allow_tls"
+#  }
+#}
