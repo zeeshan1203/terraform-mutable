f38b933dbcf54df0b49bc96e67e265fa81a6600b
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-29 20:40:30 +0530

    added & updated and modified

--- databases/data.tf
+++ databases/data.tf
@@ -1,19 +1,23 @@
 data "aws_ami" "centos7" {
   most_recent      = true
-  name_regex       = "^Centos-7-DevOps-Practice"   ##ami name
-  owners           = ["973714476881"]              ##mai owner id
+  name_regex       = "^Centos-7-DevOps-Practice"          ##ami name
+  owners           = ["973714476881"]                     ##mai owner id
 }
 
 data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config = {
-    bucket                = "terraform-szs"       	##ur bucket name
+    bucket                = "terraform-szs"       	      ##ur bucket name
     key                   = "mutable/vpc/${var.ENV}/terraform.tfstate"
     region                = "us-east-1"
   }
 }
 
-output "outputs" {
-  value = data.terraform_remote_state.vpc.outputs
+data "aws_secretsmanager_secret" "secrets" {
+  name = "${var.ENV}-env"
+}
+
+output "secrets" {
+  value = data.aws_secretsmanager_secret.secrets
 }
--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -44,3 +44,17 @@ resource "aws_security_group" "allow_mongodb" {
     Name                      = "AllowMongoDB"
   }
 }
+
+//
+//resource "null_resource" "ansible-mongo" {
+//  provisioner "local-exec" {
+//    command                   = "sleep 30"
+//  }
+//
+//  provisioner "remote-exec" {
+//    connection {
+//      host                    = aws_spot_instance_request.mongodb.private_ip
+//      user                    =
+//    }
+//  }
+//}
