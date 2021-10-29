151c5385525c87351ed8b28dc449926dea5c0dcd
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-29 16:53:27 +0530

    updated and modified

--- vpc/gateways.tf
+++ vpc/gateways.tf
@@ -5,3 +5,14 @@ resource "aws_internet_gateway" "igw" {
     Name = "igw"
   }
 }
+
+resource "aws_eip" "nat" {}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.*.id[0]
+
+  tags = {
+    Name = "nat"
+  }
+}
