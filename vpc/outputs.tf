89ae59c7bd12d914b1866bcfd10c59b1081e8d67
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-29 20:12:54 +0530

    added & updated and modified

---
+++ vpc/outputs.tf
@@ -0,0 +1,31 @@
+output "VPC_ID" {
+  value = aws_vpc.main.id
+}
+
+output "VPC_CIDR" {
+  value = aws_vpc.main.cidr_block
+}
+
+output "PRIVATE_SUBNETS" {
+  value = aws_subnet.private.*.id
+}
+
+output "PUBLIC_SUBNETS" {
+  value = aws_subnet.public.*.id
+}
+
+output "PUBLIC_SUBNET_CIDR" {
+  value = aws_subnet.public.*.cidr_block
+}
+
+output "PRIVATE_SUBNET_CIDR" {
+  value = aws_subnet.private.*.cidr_block
+}
+
+output "DEFAULT_VPC_ID" {
+  value = var.DEFAULT_VPC_ID
+}
+
+output "DEFAULT_VPC_CIDR" {
+  value = var.DEFAULT_VPC_CIDR
+}
