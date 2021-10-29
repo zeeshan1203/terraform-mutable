9d627a76a5f53b562a822868c909047813f374d4
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-29 21:03:13 +0530

    added & updated and modified

--- databases/data.tf
+++ databases/data.tf
@@ -1,14 +1,14 @@
 data "aws_ami" "centos7" {
   most_recent      = true
-  name_regex       = "^Centos-7-DevOps-Practice"          ##ami name
-  owners           = ["973714476881"]                     ##mai owner id
+  name_regex       = "^Centos-7-DevOps-Practice"
+  owners           = ["973714476881"]
 }
 
 data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config = {
-    bucket                = "terraform-szs"       	      ##ur bucket name
+    bucket                = "terraform-szs"
     key                   = "mutable/vpc/${var.ENV}/terraform.tfstate"
     region                = "us-east-1"
   }
@@ -21,11 +21,3 @@ data "aws_secretsmanager_secret" "secrets" {
 data "aws_secretsmanager_secret_version" "secrets" {
   secret_id = data.aws_secretsmanager_secret.secrets.id
 }
-
-output "secrets" {
-  value = base64encode(jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_USER"])
-}
-
-
-
-
--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -45,16 +45,28 @@ resource "aws_security_group" "allow_mongodb" {
   }
 }
 
-//
-//resource "null_resource" "ansible-mongo" {
-//  provisioner "local-exec" {
-//    command                   = "sleep 30"
-//  }
-//
-//  provisioner "remote-exec" {
-//    connection {
-//      host                    = aws_spot_instance_request.mongodb.private_ip
-//      user                    =
-//    }
-//  }
-//}
+resource "null_resource" "ansible-mongo" {
+  provisioner "local-exec" {
+    command                   = "sleep 30"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host                    = aws_spot_instance_request.mongodb.private_ip
+      user                    = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_USER"]
+      password                = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_PASS"]
+    }
+
+    inline = [
+      "sudo yum install python3-pip -y",
+      "sudo pip3 install pip --upgrade",
+      "sudo pip3 install ansible==4.1.0",
+      #      "sudo yum install ansible -y",
+      #      "sudo yum remove ansible -y",
+      #      "sudo rm -rf /usr/lib/python2.7/site-packages/ansible*",
+      #      "sudo pip install ansible",
+      "ansible-pull -i localhost, -U https://github.com/zeeshan1203/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
+    ]
+
+  }
+}
