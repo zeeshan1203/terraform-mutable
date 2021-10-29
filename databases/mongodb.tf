0a17e53f0fbd26b747406260ea0ece95a9069d09
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-29 21:07:34 +0530

    added & updated and modified

--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -61,11 +61,12 @@ resource "null_resource" "ansible-mongo" {
       "sudo yum install python3-pip -y",
       "sudo pip3 install pip --upgrade",
       "sudo pip3 install ansible==4.1.0",
+      "ansible-pull -i localhost, -U https://github.com/zeeshan1203/ansible.git roboshop-pull.yml -e COMPONENT=mongodb"
       #      "sudo yum install ansible -y",
       #      "sudo yum remove ansible -y",
       #      "sudo rm -rf /usr/lib/python2.7/site-packages/ansible*",
       #      "sudo pip install ansible",
-      "ansible-pull -i localhost, -U https://github.com/zeeshan1203/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
+      #      "ansible-pull -i localhost, -U https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps56/_git/ansible roboshop-pull.yml -e COMPONENT=mongodb"
     ]
 
   }
