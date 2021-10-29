cad89eaf92b7082f40de08db8be8e4a4e7bbab91
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-29 20:28:48 +0530

    added & updated and modified

--- databases/mongodb.tf
+++ databases/mongodb.tf
@@ -1,15 +1,16 @@
-//resource "aws_spot_instance_request" "mongodb" {
-//  ami                         = data.aws_ami.centos7.id
-//  spot_price                  = "0.0031"
-//  instance_type               = "t3.micro"
-//  vpc_security_group_ids      = []
-//
-//  tags                        = {
-//    Name                      = ""
-//  }
-//}
-//
-//
+resource "aws_spot_instance_request" "mongodb" {
+  ami                         = data.aws_ami.centos7.id
+  spot_price                  = "0.0031"
+  instance_type               = "t3.micro"
+  vpc_security_group_ids      = [aws_security_group.allow_mongodb.id]
+  subnet_id                   = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS[1]
+
+  tags                        = {
+    Name                      = "mongodb-${var.ENV}"
+    Environment               = var.ENV
+  }
+}
+
 resource "aws_security_group" "allow_mongodb" {
   name                        = "allow_mongodb"
   description                 = "AllowMongoDB"
