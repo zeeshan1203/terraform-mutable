5ec29677fa1b0fd601e62a73de6ec50e63361416
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-29 16:27:39 +0530

    updated and modified

--- vpc/route-tables.tf
+++ vpc/route-tables.tf
@@ -23,21 +23,3 @@ resource "aws_route_table" "public-rt" {
     Name          = "public-route-table"
   }
 }
-
-resource "aws_route" "route-in-default-vpc" {
-  route_table_id            = var.DEFAULT_VPC_ROUTE_TABLE
-  destination_cidr_block    = var.VPC_CIDR
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
-}
-
-resource "aws_route_table_association" "public-association" {
-  count                       = length(var.SUBNET_ZONES)
-  subnet_id                   = element(aws_subnet.public.*.id,count.index)
-  route_table_id              = aws_route_table.public-rt.id
-}
-
-resource "aws_route_table_association" "private-association" {
-  count                       = length(var.SUBNET_ZONES)
-  subnet_id                   = element(aws_subnet.private.*.id,count.index)
-  route_table_id              = aws_route_table.private-rt.id
-}
