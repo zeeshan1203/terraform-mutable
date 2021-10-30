908ea324751c5c134617c2fa3f8407df6733f775
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-10-30 15:37:56 +0530

    added & updated and modified

--- databases/elastic-cache.tf
+++ databases/elastic-cache.tf
@@ -7,10 +7,10 @@ resource "aws_elasticache_cluster" "example" {
   engine_version                        = "5.0.6"
   port                                  = 6379
   security_group_ids                    = [aws_security_group.allow_rds_mysql.id]
-  subnet_group_name                     = aws_db_subnet_group.redis-subnet-group.name
+  subnet_group_name                     = aws_elasticache_subnet_group.redis-subnet-group.name
 }
 
-resource "aws_db_subnet_group" "redis-subnet-group" {
+resource "aws_elasticache_subnet_group" "redis-subnet-group" {
   name                                  = "redis-db-group-${var.ENV}"
   subnet_ids                            = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS
 
