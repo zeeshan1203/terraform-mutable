586f921099d630623b6c9b3636b2841d172106ba
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-11-01 15:55:45 +0530

    added & updated and modified

--- instances/ec2.tf
+++ instances/ec2.tf
@@ -70,3 +70,17 @@ resource "null_resource" "ansible-apply" {
 
   }
 }
+
+resource "aws_lb_target_group" "target-group" {
+  name                       = "${var.COMPONENT}-${var.ENV}"
+  port                       = var.PORT
+  protocol                   = "HTTP"
+  vpc_id                     = data.terraform_remote_state.vpc.outputs.VPC_ID
+}
+
+resource "aws_lb_target_group_attachment" "tg-attach" {
+  count                      = var.INSTANCE_COUNT
+  target_group_arn           = aws_lb_target_group.target-group.arn
+  target_id                  = element(aws_spot_instance_request.instances.*.id, count.index)
+  port                       = var.PORT
+}
