ddd860f05d933c8e92974750c9ab4872fe14789e
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-11-01 12:10:20 +0530

    added & updated and modified

--- vpc/route-tables.tf
+++ vpc/route-tables.tf
@@ -90,3 +90,4 @@ resource "aws_route_table_association" "private-association" {
   subnet_id                       = element(aws_subnet.private.*.id,count.index)
   route_table_id                  = aws_route_table.private-rt.id
 }
+
