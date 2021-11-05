9334e7a1ec3f9a07cb7a0236ba6fbd270f04d8a4
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-11-05 16:10:19 +0530

    added & updated and modified

--- databases/elastic-cache.tf
+++ databases/elastic-cache.tf
@@ -54,4 +54,3 @@ resource "aws_route53_record" "elastic-record" {
   ttl                         = "300"
   records                     = [aws_elasticache_cluster.example.cache_nodes[0].address]
 }
-
